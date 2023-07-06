1a061aa7888ef3b7af606cab8aa0583a5b7f9504
Dan <itsdangruzdev@gmail.com>
2023-07-06 13:52:13 +0300

    Hooks added

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,9 +1,16 @@
 import { React, useState } from "react";
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+} from "react-router-dom/cjs/react-router-dom";
 import AppHeader from "../appHeader/AppHeader";
 import RandomChar from "../randomChar/RandomChar";
 import CharList from "../charList/CharList";
 import CharInfo from "../charInfo/CharInfo";
 import ErrorBoundary from "../errorBoundary/ErrorBoundary";
+import AppBanner from "../appBanner/AppBanner";
+import ComicsList from "../comicsList/ComicsList";
 
 import decoration from "../../resources/img/vision.png";
 
@@ -15,23 +22,33 @@ const App = () => {
   };
 
   return (
-    <div className="app">
-      <AppHeader />
-      <main>
-        <ErrorBoundary>
-          <RandomChar />
-        </ErrorBoundary>
-        <div className="char__content">
-          <ErrorBoundary>
-            <CharList onCharSelected={onCharSelected} />
-          </ErrorBoundary>
-          <ErrorBoundary>
-            <CharInfo charId={selectedChar} />
-          </ErrorBoundary>
-        </div>
-        <img className="bg-decoration" src={decoration} alt="vision" />
-      </main>
-    </div>
+    <Router>
+      <div className="app">
+        <AppHeader />
+        <main>
+          <Switch>
+            <Route exact path="/">
+              <ErrorBoundary>
+                <RandomChar />
+              </ErrorBoundary>
+              <div className="char__content">
+                <ErrorBoundary>
+                  <CharList onCharSelected={onCharSelected} />
+                </ErrorBoundary>
+                <ErrorBoundary>
+                  <CharInfo charId={selectedChar} />
+                </ErrorBoundary>
+              </div>
+              <img className="bg-decoration" src={decoration} alt="vision" />
+            </Route>
+            <Route exact path="/comics">
+              <AppBanner />
+              <ComicsList />
+            </Route>
+          </Switch>
+        </main>
+      </div>
+    </Router>
   );
 };
 
--- src/components/randomChar/RandomChar.js
+++ src/components/randomChar/RandomChar.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { useState, useEffect } from "react";
-import MarvelService from "../../services/MarvelService";
+import useMarvelService from "../../services/MarvelService";
 import "./randomChar.scss";
 import ErrorMessage from "../errorMessage/ErrorMessage";
 import Spinner from "../spinner/Spinner";
@@ -8,10 +8,8 @@ import mjolnir from "../../resources/img/mjolnir.png";
 
 const RandomChar = () => {
   const [char, setChar] = useState(null);
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(false);
 
-  const marvelService = new MarvelService();
+  const { loading, error, getCharacter, clearError } = useMarvelService();
 
   useEffect(() => {
     updateChar();
@@ -23,23 +21,13 @@ const RandomChar = () => {
   }, []);
 
   const onCharLoaded = (char) => {
-    setLoading(false);
     setChar(char);
   };
 
-  const onCharLoading = () => {
-    setLoading(true);
-  };
-
-  const onError = () => {
-    setError(true);
-    setLoading(false);
-  };
-
   const updateChar = () => {
+    clearError();
     const id = Math.floor(Math.random() * (1011400 - 1011000)) + 1011000;
-    onCharLoading();
-    marvelService.getCharacter(id).then(onCharLoaded).catch(onError);
+    getCharacter(id).then(onCharLoaded);
   };
 
   const errorMessage = error ? <ErrorMessage /> : null;
